73bfe2436a1da815f31a71c05a29476a7f86374d
jessicavieira761 <vieirajessica2202@gmail.com>
2026-06-04 15:21:36 -0300

    Conclui Exercício 3 de input controlado em tempo real
    Criação do componente TextoTempoReal com estado para capturar valor do input.
    
    Uso do operador lógico curto-circuito (&&) para exibição condicional do texto.
    
    Estilização isolada criada no arquivo TextoTempoReal.css e exportação padrão corrigida

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "./components/Layout";
 import Vitrine from "./components/Vitrine";
 import "./App.css";
 import Contador from "./components/Contador";
+import TextoTempoRreal from "./components/TextoTempoReal";
 
 function App() {
     return (
