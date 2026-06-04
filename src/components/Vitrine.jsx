b5de7ac684fafdbd424aa10e1f3b8eb0e657123c
jessicavieira761 <vieirajessica2202@gmail.com>
2026-06-04 16:03:16 -0300

    conclui busca dinâmica e testes de ciclo de vida com useEffect
    Implementação do estado de busca e input dinâmico no componente Vitrine.
    
    Criação de filtro em tempo real utilizando o método .includes() para exibir os produtos correspondentes.
    
    Adicionados testes práticos com o hook useEffect (com array vazio [] e com a dependência [busca]) para monitorar e compreender os gatilhos de renderização via Console.

--- src/components/Vitrine.jsx
+++ src/components/Vitrine.jsx
@@ -5,6 +5,7 @@ function Vitrine() {
     const [produtos, setProdutos] = useState([]);
     const [carregando, setCarregando] = useState(true);
     const [erro, setErro] = useState(null);
+    const [busca, setBusca] = useState("");
 
     useEffect(() => {
         fetch("https://dummyjson.com/products/category/laptops")
@@ -19,15 +20,42 @@ function Vitrine() {
         });
     }, []);
 
-    if (carregando) return <p>Carregando produtos...</p>;
-    if (erro) return <p>{erro}</p>;
+        // TESTE A: useEffect com o array vazio []
+    useEffect(() => {
+        console.log("%c[useEffect []] Fui disparado apenas UMA vez (quando a vitrine nasceu)!", "color: #3498db; font-weight: bold;");
+    }, []);
+
+    // TESTE B: useEffect com a dependência [busca]
+    useEffect(() => {
+        console.log(`%c[useEffect [busca]] Fui disparado! O texto atual da busca é: "${busca}"`, "color: #e67e22; font-weight: bold;");
+    }, [busca]);
+
+    // if de carregamento e erro vem depois de todos os hooks!
+    if(carregando) return <p>Carregando produtos...</p>;
+    if(erro) return <p>{erro}</p>;
+
+    // lista com filtro 
+    const filtrados = produtos.filter((p) =>
+        p.title.toLowerCase().includes(busca.toLocaleLowerCase())
+    );
 
     return (
-        <section className="vitrine">
-            
-            {produtos.map((p) => (
+        <section>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Busque um produto para testar o useEffect."
+                    value={busca}
+                    onChange={(e) => setBusca(e.target.value)}
+                />
+            </div>
+
+            <div className="vitrine">
+            {/* mapeando os filtrados(produtos)*/}
+            {filtrados.map((p) => (
                 <ProdutoCard key={p.id} produto={p} />
             ))}
+            </div>
         </section>
     );
 }
