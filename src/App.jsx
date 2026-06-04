3374064456d2ca0afe58efe254e312442e2ca6c8
jessicavieira761 <vieirajessica2202@gmail.com>
2026-06-04 14:40:24 -0300

    cria componente de Contador para o Exercício 1
    Criação do componente Contador utilizando o hook useState para gerenciar o estado numérico.
    
    Adicionados botões de incrementar (+) e decrementar (−).
    
    Criação e importação do arquivo Contador.css para manter a estilização isolada e o código JavaScript limpo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Layout from "./components/Layout";
 import Vitrine from "./components/Vitrine";
 import "./App.css";
+import Contador from "./components/Contador";
 
 function App() {
     return (
